615e75fd77b090ebb61273f60b8516c78454b634
Victor Gonzalez <victorggrodriguez@gmail.com>
2018-10-08 19:06:50 +0000

    automatically backed up by learn

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -1,5 +1,5 @@
 def second_challenge
-  epic_tragedy = {
+  epic_tragedy: {
     :montague => {
       :patriarch => {}.
       :matriarch => {},
@@ -12,5 +12,5 @@ def second_challenge
       :heroine => {},
       :heroine_friends => {}
       }}
-  end
+  
 end
